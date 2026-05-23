0d8907f5194a2b947259a855a4caa4e43df19d51
= <publicuser0999847695359@gmail.com>
2026-05-22 22:13:10 -0400

    feat: detect panics and exit with error dialog

--- launcher/main.go
+++ launcher/main.go
@@ -4,9 +4,24 @@ import (
 	"fmt"
 	"mc-portable-launcher/src/exp"
 	"mc-portable-launcher/src/launcherApp"
+	"mc-portable-launcher/src/ux"
+	"os"
 )
 
+func handlePanic() {
+	if r := recover(); r != nil {
+		fmt.Fprintf(os.Stderr, "Error: The application encountered a fatal issue and must exit.\n")
+		fmt.Fprintf(os.Stderr, "Details: %v\n", r)
+
+		ux.ShowError("Fatal Error Occurred", "A fatal error occurred inside the application. MCPL cannot recover.\n\nPlease report this issue.")
+
+		os.Exit(1)
+	}
+}
+
 func main() {
+
+	defer handlePanic()
 
 	exp.CheckToken()
 
